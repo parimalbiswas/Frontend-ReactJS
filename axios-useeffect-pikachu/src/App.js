55667a0668eedae323d0d27dd4ac65500580c7ab
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-06-21 21:52:59 +0530

    axios-useeffect-pikachu
    axios-useeffect-pikachu

---
+++ axios-useeffect-pikachu/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CompA from './components/CompA';
+
+function App() {
+  return (
+    <div className="App">
+      <CompA />
+    </div>
+  );
+}
+
+export default App;

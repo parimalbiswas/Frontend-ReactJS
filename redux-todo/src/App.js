309921507fb99ed0758c743a5a48798f38b12400
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-07-11 18:23:52 +0530

    .

---
+++ redux-todo/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+
+          <h3>Add your List Here</h3>
+          <div className="add-items">
+
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+
+
